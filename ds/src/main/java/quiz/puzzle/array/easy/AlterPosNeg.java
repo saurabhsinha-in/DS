09e3d27e2b5c90bc12bf4b15606577b9e103e359
sau.tut <sinhasaurabhy2k.tutorial@gmail.com>
2025-06-07 07:26:00 +0530

    ReVisit Questions

---
+++ ds/src/main/java/quiz/puzzle/array/easy/AlterPosNeg.java
@@ -0,0 +1,57 @@
+/**
+Given an unsorted array arr containing both positive and negative numbers. Your task is to rearrange the array and convert it into an array of alternate positive and negative numbers without changing the relative order.
+
+Note:
+- Resulting array should start with a positive integer (0 will also be considered as a positive integer).
+- If any of the positive or negative integers are exhausted, then add the remaining integers in the answer as it is by maintaining the relative order.
+- The array may or may not have the equal number of positive and negative integers.
+
+Examples:
+
+Input: arr[] = [9, 4, -2, -1, 5, 0, -5, -3, 2]
+Output: [9, -2, 4, -1, 5, -5, 0, -3, 2]
+Explanation: The positive numbers are [9, 4, 5, 0, 2] and the negative integers are [-2, -1, -5, -3]. Since, we need to start with the positive integer first and then negative integer and so on (by maintaining the relative order as well), hence we will take 9 from the positive set of elements and then -2 after that 4 and then -1 and so on.
+Input: arr[] = [-5, -2, 5, 2, 4, 7, 1, 8, 0, -8]
+Output: [5, -5, 2, -2, 4, -8, 7, 1, 8, 0]
+Explanation : The positive numbers are [5, 2, 4, 7, 1, 8, 0] and the negative integers are [-5,-2,-8]. According to the given conditions we will start from the positive integer 5 and then -5 and so on. After reaching -8 there are no negative elements left, so according to the given rule, we will add the remaining elements (in this case positive elements are remaining) as it in by maintaining the relative order.
+Input: arr[] = [9, 5, -2, -1, 5, 0, -5, -3, 2]
+Output: [9, -2, 5, -1, 5, -5, 0, -3, 2]
+Explanation: The positive numbers are [9, 5, 5, 0, 2] and the negative integers are [-2, -1, -5, -3]. Since, we need to start with the positive integer first and then negative integer and so on (by maintaining the relative order as well), hence we will take 9 from the positive set of elements and then -2 after that 5 and then -1 and so on.
+ */
+
+package quiz.puzzle.array.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlterPosNeg {
+    public void rearrange(ArrayList<Integer> result) {
+
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
+
+        for(int i : result){
+            if(i<0){
+                neg.add(i);
+            }else{
+                pos.add(i);
+            }
+        }
+
+        int j=0,k=0;
+        result.clear();
+        while(j<pos.size() && k< neg.size()){
+             result.add(pos.get(j++));
+             result.add(neg.get(k++));
+        }
+
+        while(j<pos.size()){
+            result.add(pos.get(j++));
+        }
+
+        while(k<neg.size()){
+            result.add(neg.get(k++));
+        }
+
+    }
+}
